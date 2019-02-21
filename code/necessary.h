82b7b67ea754daf942111dddbcf2c66651c0ac4f
Utah Xef <u25th_engineer@163.com>
2019-02-21 14:52:18 +0800

    modifying in the design report

---
+++ code/necessary.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+
+#define SampleCount 15 //学习样本个数
+#define INnum 2 //输入层神经元数目
+#define HN 15//隐层神经元数目
+#define ONnum 1 //输出层神经元数目
+
+using namespace std;
